ed462def75b052d3829b2d8765b5b655a2b4dc4a
Saul Uribe <s4ulu4@gmail.com>
2024-10-26 18:57:10 -0700

    Adding router with header parser

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 
 // init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const api = require('./myApp');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require('cors');
+const cors = require('cors');
+
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,12 +21,9 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use('/api', api());
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
---
+++ myApp.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+
+const parseHeader = (req) => {
+};
+
+const api = () => {
+    let routes = new Router();
+
+    // your first API endpoint...
+    app.get('/hello', function (req, res) {
+      res.json({ greeting: 'hello API' });
+    });
+
+    app.get('/whoami', (req, res) => {
+        let data = {
+            ipaddress: req.ip,
+            language: req.get('Accept-Language'),
+            software: req.get('User-Agent')
+        };
+
+        return res.status(200).json(data).end();
+    });
+
+    return routes;
+};
